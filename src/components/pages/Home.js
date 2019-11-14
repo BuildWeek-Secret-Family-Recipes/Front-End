0bb2a03f5f66b43b722e2f523e7760e1a5cc3601
Barbara Mayfield <bmayfield1991@gmail.com>
2019-11-14 13:00:05 -0600

    Router imported to index.js, Home, Login, Register comps routed to App.js, added some colors to App.css file

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,9 @@
 import React from 'react';
-
-import Nav from '../layout/Nav'
 import Dining from '../../assets/img/dining.png';
 
 const Home = () => {
     return (
         <div>
-            <Nav />
             <img src={Dining} alt='Family Dining' />
             <p>Coming soon!</p>
         </div>
