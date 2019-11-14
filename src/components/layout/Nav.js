7489d941b911328e419d1a2fa78b4005af966d3a
Samuel Kraus <S_read@aol.com>
2019-11-14 17:22:05 -0600

    put styled open container component template in

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -2,9 +2,51 @@ import React, { Fragment } from 'react';
 import { Link } from "react-router-dom";
 import { getToken } from '../../utils/api';
 import Search from '../layout/Search';
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
-const Nav = () => {
+
+const StyledMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #9C976A;
+  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+  height: 100vh;
+  text-align: left;
+  padding: 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: transform 0.3s ease-in-out;
+
+  @media (max-width: 576px) {
+      width: 100%;
+    }
+
+  a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: #654F3B;
+    text-decoration: none;
+    transition: color 0.3s linear;
+
+    @media (max-width: 576px) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    &:hover {
+      color: #C4C4C4;
+    }
+  }
+`
+
+
+
+const Nav = ({ open }) => {
 
     const logged = getToken();
 
