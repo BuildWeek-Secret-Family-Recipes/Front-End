6ed224a3b7102eb283b0e4d940e3e234414f5669
Samuel Kraus <S_read@aol.com>
2019-11-14 15:07:48 -0600

    updated changes to nav file

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { Link } from "react-router-dom";
+import { getToken } from '../../utils/api';
 import Search from '../layout/Search';
-import styled from 'styled-components';
+import Styled from 'styled-components';
 
 const Nav = () => {
+
+    const logged = getToken();
+
     return (
-        <div className="nav">
+        <Fragment>
+            <Link to='/'>Home</Link>
+            {logged && <Link to='/myrecipes'>My Recipes</Link>}
+            {!logged && <Link to='/api/login'>Log In</Link>}
+            {logged && <Link to='/logout'>Logout</Link>}
+            <Link to='/register'>Sign Up</Link>
             <Search />
-        </div>
+        </Fragment>
     )
 }
 
 export default Nav
-
