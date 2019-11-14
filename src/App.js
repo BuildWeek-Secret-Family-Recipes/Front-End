857d7cd726f24ad975eced8ad7f02d8fd291e20d
Barbara Mayfield <bmayfield1991@gmail.com>
2019-11-14 14:06:04 -0600

    Private routes implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import './App.css';
+import PrivateRoute from './utils/PrivateRoute';
 
 import Home from './components/pages/Home';
 import Nav from './components/layout/Nav';
 import Login from './components/user/Login';
 import Register from './components/user/Register';
+import Signout from './components/user/Signout';
+import UserRecipes from './components/recipe/UserRecipes';
 
+import './App.css';
 
 function App() {
   return (
@@ -15,8 +18,10 @@ function App() {
         <Nav />
 
         <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
+        <Route exact path='/api/login' component={Login} />
         <Route exact path='/register' component={Register} />
+        <PrivateRoute exact path='/myrecipes' component={UserRecipes} />
+			  <PrivateRoute exact path="/logout" component={Signout} />
       </div>
     </div>
   );
--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,12 +1,18 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 import { Link } from "react-router-dom";
+import { getToken } from '../../utils/api';
 import Search from '../layout/Search';
 
 const Nav = () => {
+
+    const logged = getToken();
+
     return (
         <Fragment>
             <Link to='/'>Home</Link>
-            <Link to='/login'>Log In</Link>
+            {logged && <Link to='/myrecipes'>My Recipes</Link>}
+            {!logged && <Link to='/api/login'>Log In</Link>}
+            {logged && <Link to='/logout'>Logout</Link>}
             <Link to='/register'>Sign Up</Link>
             <Search />
         </Fragment>
