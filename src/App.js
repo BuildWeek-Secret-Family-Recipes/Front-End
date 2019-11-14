bb9e4441461b285b45ba596e6f0d00e4bad69224
Samuel Kraus <S_read@aol.com>
2019-11-14 15:14:44 -0600

    updated changes from master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute';
 
 import Home from './components/pages/Home';
+import Nav from './components/layout/Nav';
+import Login from './components/user/Login';
+import Register from './components/user/Register';
+import Signout from './components/user/Signout';
+import UserRecipes from './components/user/UserRecipes';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <div className="nav">
+        <Nav />
+
+        <Route exact path='/' component={Home} />
+        <Route exact path='/api/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <PrivateRoute exact path='/myrecipes' component={UserRecipes} />
+			  <PrivateRoute exact path="/logout" component={Signout} />
+      </div>
     </div>
   );
 }
