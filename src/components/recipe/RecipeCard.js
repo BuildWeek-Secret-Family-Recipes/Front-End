afcc5ff6189b2def8c6c8a0a99509ac0c65d4852
Samuel Kraus <S_read@aol.com>
2019-11-20 21:39:15 -0600

    brought test recipe cards into home

--- src/components/recipe/RecipeCard.js
+++ src/components/recipe/RecipeCard.js
@@ -1,43 +1,73 @@
 // import React from 'react';
 
-// export default function RecipeCard() {
-//     return (
-//         <div>
-//             <h3>Recipe Card</h3>
-//         </div>
-//     )
-// }
 
-// import React, { useState, useEffect} from 'react';
-// import axios from 'axios';
-// import RecipeCards from './RecipeCards.js';
-// import { Container, Row } from 'reactstrap';
+// recipe = {
+//     name: 'string',
+//     type_of_meal: 'string',
+//     original_author: 'string',
+//     user_id: 4 //integer, will be the same number as the user's ID who is creating the recipe
+// } 
 
-// export default function RecipeCard(){
 
-//     const [recipe, setRecipe] = useState([]);
+import React from "react";
+import styled from "styled-components"
+import {device} from '../layout/Breakpoints.js';
 
-//     useEffect(() =>{
-//         axios.get(`https://secret-recipes.herokuapp.com/api/auth/recipes`)
-//         .then(response =>{
-//             setRecipe(response.data);
-//         })
-//         .catch(error => {
-//             console.log("recipes were not returned", error);
-//         });
-//     }, [])
 
-//     return(
-//         <Container>
-//             <Row>
-//                 {
-//                     recipe.map(recipeInfo =>{
-//                         return(
-//                             <RecipeCards/>
-//                         )
-//                     })
-//                 }
-//             </Row>
-//         </Container>
-//     )
-// }
+export default function RecipeCard(props) {
+
+    const Card = styled.div`
+    height: 300px;
+width: 250px;
+background-color: #C4C4C4;
+margin-bottom: 2rem;
+
+@media ${device.mobileS} {
+    width: 50%;
+    margin-left: 5%;
+
+}
+
+@media ${device.tablet}{
+    width: 45%;
+}
+
+@media ${device.laptop}{
+    width: 35%;
+}
+
+
+@media ${device.laptopL}{
+    width: 22%;
+    height: 350px;
+    margin-left: 0;
+}
+
+
+@media ${device.desktop}{
+    width: 22%;
+    height: 400px;
+}
+
+@media ${device.desktopL}{
+    width: 20%;
+    height: 440px;
+    margin-left: 1%;
+    margin-right: 1%;
+}
+
+    `
+    const Header = styled.h2`
+    font-size: 25px;
+    // text-shadow: 2px 2px BlueViolet;
+    `
+
+
+  return(
+    <Card>
+      <Header>Name{props.name}</Header>
+        <p>Type of Meal: {props.type_of_meal}</p>
+        <p>Original Author {props.original_author}</p>
+    </Card>
+  )
+}
