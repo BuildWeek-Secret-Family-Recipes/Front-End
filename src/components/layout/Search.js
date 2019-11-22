6ec8a34d142a427dcc8430deedaae3973b9fd5bd
Samuel Kraus <S_read@aol.com>
2019-11-21 21:05:23 -0600

    refactored search form

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import RecipeCards from '../recipe/RecipeCards';
-import AxiosWithAuth from '../../utils/api';
-import { Form, Field, withFormik } from 'formik';
-import { Button } from 'reactstrap';
+// import React, { useState, useEffect } from "react";
+// import RecipeCards from '../recipe/RecipeCards';
+// import AxiosWithAuth from '../../utils/api';
+// import { Form, Field, withFormik } from 'formik';
+// import { Button } from 'reactstrap';
 // import {Link} from "react-router-dom";
 // import styled from "styled-components";
 
@@ -25,100 +25,153 @@ import { Button } from 'reactstrap';
 // export default Search
 
 // export default 
-function SearchForm() {
+// function SearchForm() {
 
-    const [recipe, setRecipe] = useState([]);
-    // const [query, setQuery] = useState("");
-    const [data, setData] = useState([])
-    const [filteredRecipes, setFilteredRecipes] = useState([]);
+//     const [recipe, setRecipe] = useState([]);
+//     // const [query, setQuery] = useState("");
+//     const [data, setData] = useState([])
+//     const [filteredRecipes, setFilteredRecipes] = useState([]);
 
-    useEffect(() =>{
-        AxiosWithAuth()
-        .get(`/auth/recipes/`)
-        .then(response =>{
-            console.log(response.data)
-            // setRecipes(response.data);
-            const recipe = response.data.filter(ind => RecipeCards.name.toLowerCase())
-            setRecipe(recipe);
-        })
-        .catch(error => {
-            // console.log("recipes were not returned", error);
-        });
-    }, [data])
+//     useEffect(() =>{
+//         AxiosWithAuth()
+//         .get(`/auth/recipes/`)
+//         .then(response =>{
+//             console.log(response.data)
+//             // setRecipes(response.data);
+//             const recipe = response.data.filter(ind => RecipeCards.name.toLowerCase())
+//             setRecipe(recipe);
+//         })
+//         .catch(error => {
+//             // console.log("recipes were not returned", error);
+//         });
+//     }, [data])
       
 
-            function handleChange(e) {
-                const newRecipes = recipe.filter((data) => { 
-                  console.log(recipe, "Item")
-                 return data.RecipeCards.toLowerCase().includes(e.target.value)
-                })
-                setFilteredRecipes(newRecipes);
-              }
-              function handleSubmit(props) {
-                      setData (data);
-              }
+//             function handleChange(e) {
+//                 const newRecipes = recipe.filter((data) => { 
+//                   console.log(recipe, "Item")
+//                  return data.RecipeCards.toLowerCase().includes(e.target.value)
+//                 })
+//                 setFilteredRecipes(newRecipes);
+//               }
+//               function handleSubmit(props) {
+//                       setData (data);
+//               }
             
-              // const handleInputChange = event => {
-              //       setQuery(event.target.value);
-              //     };
+//               // const handleInputChange = event => {
+//               //       setQuery(event.target.value);
+//               //     };
 
 
 
-              return (
-                <>
-                    <Form className="searchform">
-                        <Field type="text" name="search" placeholder="Search..." className="search-field" onChange={handleChange} />
-                        <Button  onSubmit={handleSubmit}>Search</Button>
-                    </Form>
-                    {filteredRecipes.map((data) => (<p className="searchitems">{data.RecipeCards}</p>))}
-                    </>
-                );
-              }
-            export default withFormik({
-                myPropsToValues: (values) => {
-                    return {
-                        search: values.search || ''
-                    }
-                }
-            })(SearchForm)
+//               return (
+//                 <>
+//                     <Form className="searchform">
+//                         <Field type="text" name="search" placeholder="Search..." className="search-field" onChange={handleChange} />
+//                         <Button  onSubmit={handleSubmit}>Search</Button>
+//                     </Form>
+//                     {filteredRecipes.map((data) => (<p className="searchitems">{data.RecipeCards}</p>))}
+//                     </>
+//                 );
+//               }
+//             export default withFormik({
+//                 myPropsToValues: (values) => {
+//                     return {
+//                         search: values.search || ''
+//                     }
+//                 }
+//             })(SearchForm)
 
 
 
-//     const [data, setData] = useState([]);
-//     const [query, setQuery] = useState("");
+// //     const [data, setData] = useState([]);
+// //     const [query, setQuery] = useState("");
     
 
-// useEffect(() =>{
-//     AxiosWithAuth()
-//     .get(`/auth/recipes/`)
-//     .then(response =>{
-//         console.log(response.data)
-//         // setRecipes(response.data);
-//         const Recipes = response.data.filter(ind => RecipeCards.name.toLowerCase().includes(query.toLowerCase()))
-//         setData(Recipes);
-//     })
-//     .catch(error => {
-//         // console.log("recipes were not returned", error);
-//     });
-// }, [query])
+// // useEffect(() =>{
+// //     AxiosWithAuth()
+// //     .get(`/auth/recipes/`)
+// //     .then(response =>{
+// //         console.log(response.data)
+// //         // setRecipes(response.data);
+// //         const Recipes = response.data.filter(ind => RecipeCards.name.toLowerCase().includes(query.toLowerCase()))
+// //         setData(Recipes);
+// //     })
+// //     .catch(error => {
+// //         // console.log("recipes were not returned", error);
+// //     });
+// // }, [query])
   
   
-//   const handleInputChange = event => {
-//     setQuery(event.target.value);
-//   };
-//   return (
-//     <div >
-//       <form > 
-//         <input
-//         id="name" type="text" name="textfield" placeholder="Search" value={query} onChange={handleInputChange}/>
-//          <button type='submit'>Search</button>
-//       </form>
+// //   const handleInputChange = event => {
+// //     setQuery(event.target.value);
+// //   };
+// //   return (
+// //     <div >
+// //       <form > 
+// //         <input
+// //         id="name" type="text" name="textfield" placeholder="Search" value={query} onChange={handleInputChange}/>
+// //          <button type='submit'>Search</button>
+// //       </form>
   
-//       {data.map((ind => {
-//     return(<RecipeCards name={ind.name} type of meal={ind.type_of_meal} original author ={ind.original_author} />)
-//   }
-//   ))}
+// //       {data.map((ind => {
+// //     return(<RecipeCards name={ind.name} type of meal={ind.type_of_meal} original author ={ind.original_author} />)
+// //   }
+// //   ))}
   
-//   </div>
-//   )}
+// //   </div>
+// //   )}
+  
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
+import RecipeCards from '../recipe/RecipeCards';
+import AxiosWithAuth from '../../utils/api';
+import RecipeCard from "../recipe/RecipeCard";
+// import styled from "styled-components";
+// import CharacterCard from "./CharacterCard";
+// import {Link} from "react-router-dom";
+
+
+
+export default function SearchForm() {
+
+    const [data, setData] = useState([]);
+    const [query, setQuery] = useState("");
+  
+    useEffect(() =>{
+            AxiosWithAuth()
+            .get(`/auth/recipes/`)
+            .then(response =>{
+                // console.log(response.data)
+                const Recipe = response.data.filter(ind => RecipeCard.name.toLowerCase().includes(query.toLowerCase())
+    );
+    
+    setData (Recipe);
+  });
+},[query]);
+                
+       
+  
+  
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+  return (
+    <div >
+      <form > 
+        <input
+        id="name" type="text" name="textfield" placeholder="Search" value={query} onChange={handleInputChange}/>
+  
+  
+        <button type='submit'>Search</button>
+     
+      </form>
+  
+      {data.map((ind=> {
+    return(<RecipeCard name={ind.name} type of meal={ind.type_of_meal} original author={ind.original_author}/>)
+  }
+  ))}
+  
+  </div>
+  )}
   
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import Search from '../layout/Search';
 import { device } from'../layout/Breakpoints';
 import RecipeCard from '../recipe/RecipeCard';
+import RecipeCards from'../recipe/RecipeCards';
 
 export const Card = styled.div`
 height: 300px;
@@ -93,22 +94,9 @@ const Home = () => {
             <div>
                 <img src={Dining} alt='Family Dining' />
                 <CardHolder className='darkMode'>
-                    {/* <Card>Public Recipe Preview</Card>
-                    <Card>Public Recipe Preview</Card>
-                    <Card>Public Recipe Preview</Card>
-                    <Card>Public Recipe Preview</Card>
-                    <Card>Public Recipe Preview</Card>
-                    <Card>Public Recipe Preview</Card>
-                    <Card>Public Recipe Preview</Card>
-                    <Card>Public Recipe Preview</Card> */}
-                    <RecipeCard/>
-                    <RecipeCard/>
-                    <RecipeCard/>
-                    <RecipeCard/>
-                    <RecipeCard/>
-                    <RecipeCard/>
-                    <RecipeCard/>
-                    <RecipeCard/>
+                 
+                    <RecipeCards/>
+                  
                 </CardHolder>
             </div>
          
